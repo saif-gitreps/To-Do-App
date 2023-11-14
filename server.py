ef2764f115f0bfecda0b218ab0684cabf2aa133e
Saif <droconos99@gmail.com>
2023-11-14 20:54:36 +0600

    added patch method

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path , Body
+from fastapi import FastAPI, Body
 from datetime import datetime as dt
 from pydantic import BaseModel
 import json
@@ -8,6 +8,7 @@ dataFile = "database.json"
 
 class Task(BaseModel):
     task : str
+    date : str
 
 @app.get("/tasks")
 def see_tasks():
@@ -22,31 +23,56 @@ def add_new_task(task: str):
     
     currentDate = dt.now().strftime("%d-%m-%Y")
     
-    dataMap["counter"] += 1
-    dataMap[dataMap["counter"]]=  {"task": task,"date":currentDate}    
+    dataMap["counter(unrelated)"] += 1
+    dataMap[dataMap["counter(unrelated)"]]=  {"task": task,"date":currentDate}    
     
     with open(dataFile,"w") as file:
         json.dump(dataMap,file)
     return {"message":"task added succesfully"}
 
-@app.put("/task/update")
+@app.put("/task/update/{task_id}")
 def update_task(task_id: str, updateTask : Task):
     with open(dataFile,"r") as file:
         dataMap = json.load(file)
-    if task_id not in dataMap or task_id < 0:
+        
+    if task_id not in dataMap or int(task_id) < 0:
         return {"message" : "no such task"}
-    dataMap[task_id]["task"] = updateTask.task
+    dataMap[task_id] = updateTask.dict()
+    
     with open(dataFile, "w") as file:
         json.dump(dataMap,file)
     return {"message":"task updated successfully"}
 
-@app.delete("/task/delete")
+class PatchTask(BaseModel):
+    task : str = None
+    date: str = None
+
+@app.patch("/task/patch/{task_id}")
+def patch_task(task_id: str, task: PatchTask):
+    with open(dataFile, "r") as file:
+        dataMap = json.load(file)
+    
+    if task_id not in dataMap:
+        return {"message" : "no such tasks"}
+    
+    if task.task != None:
+        dataMap[task_id]["task"] = task.task
+    if task.date != None:
+        dataMap[task_id]["date"] = task.date
+    
+    with open(dataFile, "w") as file:
+        json.dump(dataMap,file)
+    return {"message" : "task patched successfully"}
+
+@app.delete("/task/delete/{task_id}")
 def delete_task(task_id: str):
     with open(dataFile,"r") as file:
         dataMap = json.load(file)
+    
     if task_id not in dataMap:
         return {"message": "no such tasks"}
     del dataMap[task_id]
+    
     with open(dataFile, "w") as file:
         json.dump(dataMap, file)
     return {"message" : "task deleted successfully"}
