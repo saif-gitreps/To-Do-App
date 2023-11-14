3204725796f8bf6e7b4944efb06b791d9c144431
Saif <droconos99@gmail.com>
2023-11-14 17:37:39 +0600

    added put method

--- server.py
+++ server.py
@@ -7,9 +7,7 @@ app = FastAPI()
 dataFile = "database.json"
 
 class Task(BaseModel):
-    taskId: int
     task : str
-    date : str
 
 @app.get("/tasks")
 def see_tasks():
@@ -20,15 +18,31 @@ def see_tasks():
 @app.post("/tasks/new-task")
 def add_new_task(task: str):
     with open(dataFile,"r") as file:
-        dataArr = json.load(file)
+        dataMap = json.load(file)
     
-    currentDate = dt.now().strftime("%d-%m-%Y") 
-    dataArr.append({"task id": len(dataArr)+1,"task": task,"date":currentDate})
+    currentDate = dt.now().strftime("%d-%m-%Y")
+    
+    if len(dataMap)+1 in dataMap:
+        dataMap[len(dataMap)+2]=  {"task": task,"date":currentDate}    
+    else: 
+        dataMap[len(dataMap)+1]=  {"task": task,"date":currentDate}
     
     with open(dataFile,"w") as file:
-        json.dump(dataArr,file)
+        json.dump(dataMap,file)
     return {"message":"task added succesfully"}
 
+@app.put("/task/update")
+def update_task(task_id: str, updateTask : Task):
+    with open(dataFile,"r") as file:
+        dataMap = json.load(file)
+    if task_id not in dataMap or task_id < 0:
+        return {"message" : "no such task exists"}
+    dataMap[task_id]["task"] = updateTask.task
+    with open(dataFile, "w") as file:
+        json.dump(dataMap,file)
+    return {"message":"task updated successfully"} 
+
+     
 
 
     
